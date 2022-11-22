45b2d64e1647e5e22ba78193fbb0005fc4ba4748
RN <navaroza.reniel@gmail.com>
2022-11-22 16:56:39 +0800

    first commit

---
+++ middleware/authentication.js
@@ -0,0 +1,26 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
+
+const auth = async (req, res, next) => {
+    // check for the header
+
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new UnauthenticatedError('Authentication Invalid')
+    }
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        const testUser = payload.userId === '6377362deeb0c95cd09aed25';;
+        // attach the user to job routes
+        req.user = { userId: payload.userId, testUser }
+        next()
+    } catch (error) {
+        throw new UnauthenticatedError('Authentication Invalid')
+    }
+
+}
+
+module.exports = auth
---
+++ routes/jobs.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const testUser = require('../middleware/testUser')
+
+const {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+    showStats
+} = require('../controllers/jobs')
+router.route('/').get(getAllJobs).post(testUser, createJob)
+router.route('/stats').get(showStats)
+router.route('/:id').get(getJob).delete(testUser, deleteJob).patch(testUser, updateJob)
+
+module.exports = router
